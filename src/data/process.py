d3fb9469e2dac40477e236c507820b86ab4766fd
sulcer <gregor.sulcer@student.um.si>
2024-05-29 13:33:20 +0200

    fix: refactore validation

--- src/data/process.py
+++ src/data/process.py
@@ -26,9 +26,9 @@ def merge_data(df, df_weather, target_feature, features, output_file_name):
     df['date'] = pd.to_datetime(df['date'])
     df_weather['date'] = pd.to_datetime(df_weather['date'])
 
-    merged_df = pd.merge(df, df_weather, on='date')
+    validate_merge(24, df, output_file_name)
 
-    # validate_merge(24, df, target_feature)
+    merged_df = pd.merge(df, df_weather, on='date')
 
     selected_features = [target_feature] + features
 
@@ -39,21 +39,21 @@ def merge_data(df, df_weather, target_feature, features, output_file_name):
         merged_df[selected_features].to_csv(output_file, index=False)
 
 
-def validate_merge(window_size, df, feature):
-    check_data = []
-    for file in os.listdir('data/processed'):
-        if file.startswith(f"{feature}_") and file.endswith('.csv'):
-            check_data = pd.read_csv(f'data/processed/{file}')
-            break
+def validate_merge(window_size, df, file_name):
+    print(f"[INFO] {file_name}")
+    check_data_path = f'data/processed/{file_name}'
+    if os.path.exists(check_data_path):
+        check_data = pd.read_csv(check_data_path)
+    else:
+        check_data = []
 
-    data_length = len(df)
-    num_of_fetches = data_length / window_size
-    new_num_of_fetches = len(check_data) / window_size
+    num_of_processed_fetches = len(check_data) / window_size
+    num_of_fetches = len(df) / window_size
 
+    print("[INFO] Number of processed fetches", num_of_processed_fetches)
     print("[INFO] Number of fetches", num_of_fetches)
-    print("[INFO] Number of new fetches", new_num_of_fetches)
 
-    if new_num_of_fetches >= num_of_fetches:
+    if num_of_fetches <= num_of_processed_fetches:
         raise ValueError("Cannot merge data, if no new data has been fetched")
 
 
