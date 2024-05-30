deec0a28a03d04b740a32db8d297450c6f65a7ee
sulcer <gregor.sulcer@student.um.si>
2024-05-30 08:07:26 +0200

    feat: update settings

--- src/config.py
+++ src/config.py
@@ -5,6 +5,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     window_size: int = 24
 
+    # dagshub configuration
+    mlflow_tracking_username: str
+    mlflow_tracking_uri: str
+    mlflow_tracking_password: str
+    dagshub_user_token: str
+    dagshub_repo_name: str
+
     __project_root = pathlib.Path(__file__).resolve().parent.parent
 
     model_config = SettingsConfigDict(env_file=f"{__project_root}/.env")
