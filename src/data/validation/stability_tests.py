e64c7b4493fd0c322041944866b76f0b7062fa22
sulcer <gregor.sulcer@student.um.si>
2024-05-31 10:31:42 +0200

    feat: add stability tests

---
+++ src/data/validation/stability_tests.py
@@ -0,0 +1,23 @@
+import os
+
+import pandas as pd
+from evidently.test_preset import DataStabilityTestPreset
+from evidently.test_suite import TestSuite
+
+if __name__ == "__main__":
+    tests = TestSuite(tests=[
+        DataStabilityTestPreset()
+    ])
+
+    for file in os.listdir("data/processed"):
+        if not file.startswith("reference_") and file.endswith(".csv"):
+            tests_subject = file.replace(".csv", "")
+
+            print(f"[INFO]: Running Stability test for {tests_subject}")
+
+            current_data = pd.read_csv(f"data/processed/{file}")
+            reference_data = pd.read_csv(f"data/processed/reference_{file}")
+
+            tests.run(reference_data=current_data, current_data=reference_data)
+
+            tests.save_html(f"reports/sites/{tests_subject}_stability_tests.html")
