ba98cddd58e105270a5801f4e5508c00f365a556
sulcer <gregor.sulcer@student.um.si>
2024-06-03 13:13:27 +0200

    fix: linter

--- src/serve/routers/prediction.py
+++ src/serve/routers/prediction.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, HTTPException
-from src.config import settings
 from src.serve.helpers.common import get_model_types
 from src.serve.services.ml_service import MLService
 
@@ -15,7 +14,9 @@ def predict(model_type: str, n_time_units: int):
         raise HTTPException(status_code=400, detail=f"Model type {model_type} not found")
 
     if n_time_units < 1 or n_time_units > 24:
-        raise HTTPException(status_code=400, detail=f"Number of future time units must be between 1 and 24")
+        raise HTTPException(
+            status_code=400,
+            detail=f"Number of future time units must be between 1 and 24. Got {n_time_units}")
 
     ml_service = MLService()
     predictions = ml_service.predict(model_type, n_time_units)
